e967b59de0ddea2fe4c1712973d7ca3e68b14e87
“yuyongtao” <3256515381@qq.com>
2026-03-08 13:33:34 +0800

    初始化项目

---
+++ src/main/java/com/recipe/srback/SrBackApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.srback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recipe/srback/SrBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.srback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
